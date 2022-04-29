bd9b02c3b044ec3d9795bfc749087fa7aa0a2d53
Kaio Prates <kaio_prates@hotmail.com>
2022-04-29 01:28:45 -0300

    Add dark/light theme functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,54 @@
+// COLOR MODE STYLE
 const body = document.querySelector('.body');
-const dark = document.querySelector('.select-dark');
-const light = document.querySelector('.select-light');
+const darkIcon = document.querySelector('.dark-color-mode');
+const lightIcon = document.querySelector('.light-color-mode');
 
-dark.addEventListener('click', () => {
-  body.classList.add("dark");
-  body.classList.remove("light");
+lightIcon.addEventListener('click', () => {
+  lightIcon.style.display = 'none';
+  darkIcon.style.display = 'block';
+  body.classList.add('light');
+  body.classList.remove('dark');
 })
 
-light.addEventListener('click', () => {
-  body.classList.add("light");
-  body.classList.remove("dark");
+darkIcon.addEventListener('click', () => {
+  darkIcon.style.display = 'none';
+  lightIcon.style.display = 'block';
+  body.classList.add('dark');
+  body.classList.remove('light');
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
