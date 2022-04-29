35663e32e4c25eac4df053a7c957d9ccfc729550
Kaio Prates <kaio_prates@hotmail.com>
2022-04-29 03:20:57 -0300

    Create basic functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,62 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function percentage(number, percentage) {
+  if(operator === 'multiply' || operator === 'division') {
+    return percentage / 100
+  } 
+  else if(operator === 'add' || operator === 'subtract') {
+    return number * (percentage / 100)
+  }
+
+} 
+
+function exponentiation(a, b = 2) {
+  return a ** b
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // COLOR MODE STYLE
 const body = document.querySelector('.body');
 const darkIcon = document.querySelector('.dark-color-theme');
