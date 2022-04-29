a4db182e157a359a0c5dd9e9869a72007fc63a05
Kaio Prates <kaio_prates@hotmail.com>
2022-04-29 17:13:28 -0300

    Implement basic calculations functionalities

--- js/script.js
+++ js/script.js
@@ -4,61 +4,116 @@ const numbersPad = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const clearBtn = document.querySelector('.clear');
 const backspaceBtn = document.querySelector('.backspace');
+const operate = document.querySelector('.equal');
 
+let dotCounter = 0;
 let displayArray = [];
+let fromIndex = 1;
+let toIndex = 0;
 
-clearBtn.addEventListener('click', () => {
+function clearDisplay() {
   displayArray = [];
-  numberDisplay.value = displayArray.join("");
-})
+  expressionDisplay.value = '';
+  numberDisplay.value = 0;
+  dotCounter = 0;
+}
 
+clearBtn.addEventListener('click', clearDisplay)
 
-backspaceBtn.addEventListener('click', () => {
+function backspace() {
   displayArray.pop();
+  dotCounter = displayArray.length; 
+    numberDisplay.value = displayArray.join("");
+    if(displayArray.length < 1) {
+      numberDisplay.value = 0;
+    }
+}
+
+backspaceBtn.addEventListener('click', backspace)
+
+function moveArrayValues(array, from, to){
+  // remove value form array and store it in a variable
+  const value = array.splice(from, 1);
+  // add removed value to another array position
+  array.splice(to, 0, value);
+}
+
+function displayNumber(number) {
+  const key = number.getAttribute('data-key');
+  displayArray.push(key);
   numberDisplay.value = displayArray.join("");
-})
-
-
+}
 
 numbersPad.forEach((number) => {
-  number.addEventListener('click', () => {
-    const key = number.getAttribute('data-key');
-    displayArray.push(key);
-    numberDisplay.value = displayArray.join("");
+  number.addEventListener('click', function() {
+    if(displayArray.length < 18) {
+      displayNumber(this)
+    } else {
+      return
+    }
   })
 });
+
 
 operators.forEach((operator) => {
   operator.addEventListener('click', () => {
     const key = operator.getAttribute('data-key');
-    displayArray.push(key);
-    numberDisplay.value = displayArray.join("");
+    expressionDisplay.value = `${displayArray.join("")} ${key} `;
+    displayArray = [];
+    dotCounter = 0;
   })
 })
 
+operate.addEventListener('click', () => {
+  expressionDisplay.value += `${displayArray.join("")} =`;
+  const expression = expressionDisplay.value;
+  const operands = expression.replace(/[+\-÷x]/g, ",").replace(/[= ]/g, "").split(",");
+  const operator = expression.match(/[+\-÷x]/g).join("");
+  switch(operator) {
+    case '+':
+      return add(operands);
+      break;
+    case '-':
+      return subtract(operands);
+      break;
+    case 'x':
+      return multiply(operands);
+      break;
+    case '÷':
+      return divide(operands);
+  }
+})
 
-
-
-
-
-
-
-
-
-function add(a, b) {
-  return a + b
+function add(operands) {
+  const calc = operands.reduce((total, operand) => {
+    return total += +operand
+  },0);
+  numberDisplay.value = calc;
+  displayArray = [];
 }
 
-function subtract(a, b) {
-  return a - b
+function subtract(operands) {
+  const calc = operands.reduce((first, second) => {
+    return first - second
+  });
+  numberDisplay.value = calc;
+  displayArray = [];
 }
 
-function multiply(a, b) {
-  return a * b
+function multiply(operands) {
+  const calc = operands.reduce((first, second) => {
+    return first * second
+  });
+  numberDisplay.value = calc;
+  displayArray = [];
 }
 
-function divide(a, b) {
-  return a / b
+function divide(operands) {
+  const calc = operands.reduce((first, second) => {
+    return first / second
+  });
+  numberDisplay.value = calc;
+  displayArray = [];
 }
 
 function percentage(number, percentage) {
